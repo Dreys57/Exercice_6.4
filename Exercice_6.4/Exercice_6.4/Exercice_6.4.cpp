06b3a2dc3ef3d78ed93334808b138bcc3dfdc6fd
Vincent <skriosdreys@gmail.com>
2019-09-30 11:01:38 +0200

    Adding the Instructions function to tell the players how to play the game

--- Exercice_6.4/Exercice_6.4/Exercice_6.4.cpp
+++ Exercice_6.4/Exercice_6.4/Exercice_6.4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 /*	Écrire une fonction void GenerateMap() qui remplit une fois la map avec un char pour représenter que les cases sont vide
 
@@ -13,7 +14,18 @@
 
 	Appeler GenerateMap() et PlayGame() in Main()*/
 
+void Instructions()
+{
+	std::cout << "\t Welcome to the Tic-Tac-Toe Game!\n\n\n";
+	std::cout << "You play by entering a number between 0 and 8 to decide the position you want to place your piece as shown by the grid below:\n\n";
+	std::cout << "0 | 1 | 2\n";
+	std::cout << "---------\n";
+	std::cout << "3 | 4 | 5\n";
+	std::cout << "---------\n";
+	std::cout << "6 | 7 | 8\n\n";
 
+	std::cout << "Let the game begin!\n\n";
+}
 
 void GenerateMap() /*Vincent*/
 {
@@ -42,7 +54,7 @@ void PlayGame() /* ? */
 
 int main()
 {
-
+	Instructions();
 
 
 
