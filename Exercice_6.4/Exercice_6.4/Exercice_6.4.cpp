b38bf1eb10d003ed328be3375e64e352749d0f08
Gael <gael.henry.sae@gmail.com>
2019-09-30 16:09:47 +0200

    ajout des fonctions : SetNewPosition et CheckWin. code non terminé

--- Exercice_6.4/Exercice_6.4/Exercice_6.4.cpp
+++ Exercice_6.4/Exercice_6.4/Exercice_6.4.cpp
@@ -54,12 +54,62 @@ void ShowMap(const std::vector<char>& board) /*Vincent*/
 
 void SetNewPosition() /*Gael*/
 {
+	int board;
+	bool coordinated = false;
 
+	do {
+		int playerNumber = 1;
+		int squareLine = 0;
+		int squareColumn = 0;
+		int compteur = 0;
+
+		system("cls"); //Clear la console
+		compteur++;
+
+		std::cout << "Player " << playerNumber << " : \n";
+		do {
+			std::cout << "Choise the line of the square   : ";
+			std::cin >> squareLine;
+			std::cout << "choise the column of the square : ";
+			std::cin >> squareColumn;
+
+		} while (coordinated != true(board, squareLine, squareColumn));
+
+		if (playerNumber == 1)
+			board[squareLine - 1][squareColumn - 1] = 'x';
+		else
+			board[squareLine - 1][squareColumn - 1] = 'o';
+
+
+		playerNumber = playerNumber % 2 + 1;
+	}
+
+	bool (coordinated = true, int x, int y)
+	{
+		if (x <= 3 && x >= 1 && y <= 3 && y >= 1) {
+			if (grille[x - 1][y - 1] != '_')
+				cout << "Case deja pleine !  " << endl << endl;
+			else
+				return true;
+		}
+		else
+			cout << "Coordonees Invalide ! " << endl << endl;
+
+		return false;
+	}
 }
 
 void CheckWin() /*Gael*/
-{
+{ 
+	bool winner = true;
+	while (winner(board) && round != 9);
+	system("cls");
 
+	playerNumber = playerNumber % 2 + 1;
+	if (winner(board))
+		std::cout << "the player " << playerNumber << " as win the game.\n\n";
+	else
+		std::cout << "the game is a egality !\n\n";
 }
 
 void PlayGame(std::vector<char>& board) /* Vincent */
@@ -78,6 +128,7 @@ void PlayGame(std::vector<char>& board) /* Vincent */
 
 int main()
 {
+
 	std::vector<char> board;
 
 	GenerateMap(board);
