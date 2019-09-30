de62212c431b1b046ee39c7ab2a284087c9fb7ed
Vincent <skriosdreys@gmail.com>
2019-09-30 10:20:56 +0200

    Adding the base program with all the functions names

---
+++ Exercice_6.4/Exercice_6.4/Exercice_6.4.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+/*	Écrire une fonction void GenerateMap() qui remplit une fois la map avec un char pour représenter que les cases sont vide
+
+	Créer une fonction  ShowMap() qui affiche la carte à son état actuel
+
+	Créer une fonction SetNewPosition(), elle demande au player une position et mets à jours la map
+
+	Créer une fonction void CheckWin() vérifie s’il y a un pattern gagnant sur la map
+
+	Créer une fonction void PlayGame() (appel ShowMap & SetNewPosition())
+
+	Appeler GenerateMap() et PlayGame() in Main()*/
+
+
+
+void GenerateMap() /*Vincent*/
+{
+
+}
+
+void ShowMap() /*Vincent*/
+{
+
+}
+
+void SetNewPosition() /*Gael*/
+{
+
+}
+
+void CheckWin() /*Gael*/
+{
+
+}
+
+void PlayGame() /* ? */
+{
+
+}
+
+int main()
+{
+
+
+
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
